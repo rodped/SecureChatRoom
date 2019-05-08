9c85c7a89dbf21013dd9756bca8ac0d69283dce1
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-05-08 11:34:42 +0100

    Template da aplicação

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import UsernameForm from './components/UsernameForm'
+
+class App extends Component {
+  render() {
+    return <UsernameForm onSubmit={username => alert(username)} />
+  }
+}
+
+export default App
