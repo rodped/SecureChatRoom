9c3f0d5060c8aaeb2c0ce0504c00a158d94d2dfd
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-05-28 23:23:08 +0100

    Nova versão

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:1c60e6e2-9aba-4a6e-a6de-11ea9db8762a',
+  key:
+    'f9901d18-b911-4f51-8340-8520b10e0aa5:3coalA2tQLUrfr2kwty6gnnFFWID17X8CbgIqmwKn3Q='
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,38 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class UsernameForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: ''
-        }
-
-        this.onChange = this.onChange.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+class UsernameForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
     }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
 
-    onChange(e) {
-        this.setState({
-            username: e.target.value,
-        })
-    }
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state.username)
+  }
 
-    onSubmit(e) {
-        e.preventDefault()
-        this.props.onSubmit(this.state.username)
-    }
+  onChange(e) {
+    this.setState({ username: e.target.value })
+  }
 
-    render() {
-        return <div>
-            <form onSubmit={this.onSubmit}>
-                <input
-                    type="text"
-                    placeholder="What is your username?"
-                    onChange={this.onChange} />
-                <input type="submit" />
-            </form>
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>What is your usernane?</h2>
+          <form onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              placeholder="Your full name"
+              onChange={this.onChange}
+            />
+            <input type="submit" />
+          </form>
         </div>
-    }
+      </div>
+    )
+  }
 }
 
 export default UsernameForm
